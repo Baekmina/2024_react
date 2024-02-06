0be73368f11505feb9252633eca6a71a725d8c9b
Baekmina <koyoa12@naver.com>
2024-02-06 18:31:48 +0900

    Improving finishing components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing); // 상태 변경 시 이전의 상태값에 기반하여 변경한다면 'setIsEditing(() => )' 이러한 함수 형태를 사용해야함.
     // setIsEditing(!isEditing) // => schedules a state update to true
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(e) {
